fffc0275a4c72c5d1a2749a0ad7318eda32fe35d
ecbDeveloper <developer.ecb@gmail.com>
2025-10-05 23:28:49 -0300

    criawebsocket basico

---
+++ src/modules/review-request/review-request.gateway.ts
@@ -0,0 +1,80 @@
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server, Socket } from 'socket.io'
+import { UsersService } from "../users/users.service";
+import { forwardRef, Inject } from "@nestjs/common";
+
+@WebSocketGateway({
+  cors: { origin: '*' }
+})
+export class ReviewRequestGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+  ) { }
+  @WebSocketServer()
+  server: Server
+
+  private connectedDevs = new Map<string, string>();
+
+  async handleConnection(socketClient: Socket) {
+    const userId = socketClient.handshake.query.userId as string;
+
+    if (userId) {
+      const user = await this.usersService.findOneById(userId)
+      if (!user) {
+        console.log('User not found')
+        return
+      }
+
+      this.connectedDevs.set(userId, socketClient.id);
+      console.log(`Client connected: ${socketClient.id} | User: ${userId}`);
+    } else {
+      console.log(`Client disconnected: ${socketClient.id} (No userId provided)`);
+    }
+  }
+
+  async handleDisconnect(socketClient: Socket) {
+    for (const [userId, socketId] of this.connectedDevs.entries()) {
+      if (socketId === socketClient.id) {
+        this.connectedDevs.delete(userId)
+        await socketClient.leave('work-room')
+
+        console.log(`Socket client with id - ${userId} desconnected from room successfully`)
+        break
+      }
+    }
+  }
+
+  async addToWorkRoom(userId: string) {
+    const socketId = this.connectedDevs.get(userId)
+    if (!socketId) {
+      console.log('User not found on connectedDevs')
+      return
+    }
+
+    const socketClient = this.server.sockets.sockets.get(socketId)
+    if (socketClient) {
+      await socketClient.join('work-room')
+      console.log(`Socket client with id - ${userId} connected from room successfully`)
+    }
+  }
+
+  async removeFromWorkRoom(userId: string) {
+    const socketId = this.connectedDevs.get(userId);
+    if (!socketId) {
+      console.log('User not found on connectedDevs')
+      return
+    }
+
+    const socketClient = this.server.sockets.sockets.get(socketId);
+    if (socketClient) {
+      await socketClient.leave('work-room');
+      this.connectedDevs.delete(userId);
+      console.log(`Socket client with id - ${userId} disconnected from room successfully`)
+    }
+  }
+
+  broadcastToRoom(room: string, event: string, payload: any) {
+    this.server.to(room).emit(event, payload);
+  }
+} 
