a89391e32531dc762086e525b5d38e64017af6fc
ecbDeveloper <developer.ecb@gmail.com>
2025-10-05 23:28:07 -0300

    altera criação de review request para q a review seja enviada para os devs present na room do socket

--- src/modules/review-request/review-request.repository.ts
+++ src/modules/review-request/review-request.repository.ts
@@ -1,6 +1,24 @@
 import { Injectable } from "@nestjs/common";
+import { CreateReviewRequestDto } from "./dto/create-review-request.dto";
+import { ReviewRequest } from "./entities/review-request.entity";
+import { Op } from 'sequelize'
 
 @Injectable()
 export class ReviewRequestRepository {
+  async create(createReviewRequestDto: CreateReviewRequestDto) {
+    const reviewRequest = await ReviewRequest.create(createReviewRequestDto)
 
+    return reviewRequest.id
+  }
+
+  async findReviewRequestInProgressByUser(userId: string) {
+    return await ReviewRequest.findOne({
+      where: {
+        userId,
+        status: {
+          [Op.in]: [1, 2]
+        }
+      }
+    })
+  }
 }
