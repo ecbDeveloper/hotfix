3b07f046e350fa35adf1de72321c371ab0b1ed41
ecbDeveloper <developer.ecb@gmail.com>
2025-10-10 15:57:06 -0300

    ajusta migration

--- src/database/migrations/20251003151903_create_review_request_table.ts
+++ src/database/migrations/20251003151903_create_review_request_table.ts
@@ -23,6 +23,6 @@ export async function up(knex: Knex): Promise<void> {
 
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists('review_request')
+  await knex.schema.dropTableIfExists('review_requests')
 }
 
