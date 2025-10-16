d037a1623d99d71cc121a6a5527b0f8844bcc37f
ecbDeveloper <developer.ecb@gmail.com>
2025-10-16 14:55:46 -0300

    adiciona comments resource

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -5,25 +5,32 @@ import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Controller('comments')
 export class CommentsController {
-  constructor(private readonly commentsService: CommentsService) {}
+  constructor(private readonly commentsService: CommentsService) { }
 
   @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
+  create(
+    @Body() createCommentDto: CreateCommentDto
+  ) {
     return this.commentsService.create(createCommentDto);
   }
 
   @Get()
-  findAll() {
-    return this.commentsService.findAll();
+  findAllBySolution() {
+    return this.commentsService.findAllBySolution();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(
+    @Param('id') id: string
+  ) {
     return this.commentsService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateCommentDto: UpdateCommentDto
+  ) {
     return this.commentsService.update(+id, updateCommentDto);
   }
 
--- src/modules/comments/comments.service.ts
+++ src/modules/comments/comments.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { CommentsRepository } from './comments.repository';
 
 @Injectable()
 export class CommentsService {
+  constructor(
+    private readonly commentsRepository: CommentsRepository
+  ) { }
+
   create(createCommentDto: CreateCommentDto) {
     return 'This action adds a new comment';
   }
 
-  findAll() {
+  findAllBySolution() {
     return `This action returns all comments`;
   }
 
