781cd3e7a670ea0d95b8fbc3e5ab917262014f5f
ecbDeveloper <developer.ecb@gmail.com>
2025-10-19 02:52:19 -0300

    ajuste por conflito

--- src/modules/accept-review/accept-review.repository.ts
+++ src/modules/accept-review/accept-review.repository.ts
@@ -6,72 +6,72 @@ import { Op } from "sequelize";
 
 @Injectable()
 export class AcceptReviewRepository {
-	async create(createAcceptReview: CreateAcceptReview) {
-		return await AcceptReview.create(createAcceptReview)
-	}
+  async create(createAcceptReview: CreateAcceptReview) {
+    return await AcceptReview.create(createAcceptReview)
+  }
 
-	async updateAcceptReviewProgress(updateAcceptReview: UpdateAcceptReviewDto) {
-		const { devId, reviewId, acceptReviewStatus } = updateAcceptReview
-		return await AcceptReview.update({ status: acceptReviewStatus }, {
-			where: { reviewId, devId }
-		})
-	}
+  async updateAcceptReviewProgress(updateAcceptReview: UpdateAcceptReviewDto) {
+    const { devId, reviewId, acceptReviewStatus } = updateAcceptReview
+    return await AcceptReview.update({ status: acceptReviewStatus }, {
+      where: { reviewId, devId }
+    })
+  }
 
-	async findOneByDevAndReviewId(reviewId: string, devId: string) {
-		return await AcceptReview.findOne({
-			where: { reviewId, devId }
-		})
-	}
+  async findOneByDevAndReviewId(reviewId: string, devId: string) {
+    return await AcceptReview.findOne({
+      where: { reviewId, devId }
+    })
+  }
 
-	async findAllByDev(devId: string) {
-		return await AcceptReview.findAll({
-			where: {
-				devId, status: {
-					[Op.in]: [2, 4]
-				}
-			}
-		})
-	}
+  async findAllByDev(devId: string) {
+    return await AcceptReview.findAll({
+      where: {
+        devId, status: {
+          [Op.in]: [2, 4]
+        }
+      }
+    })
+  }
 
-	async findInProgress(reviewId: string) {
-		return await AcceptReview.findOne({
-			where: {
-				reviewId, status: {
-					status: {
-						[Op.in]: [AcceptReviewStatuses.ACCEPTED, AcceptReviewStatuses.COMPLETED],
-					},
-				}
-			}
-		})
-	}
+  async findInProgress(reviewId: string) {
+    return await AcceptReview.findOne({
+      where: {
+        reviewId, status: {
+          status: {
+            [Op.in]: [AcceptReviewStatuses.ACCEPTED, AcceptReviewStatuses.COMPLETED],
+          },
+        }
+      }
+    })
+  }
 
-	async rejectAllPending(reviewId: string) {
-		return await AcceptReview.update({
-			status: AcceptReviewStatuses.REJECTED
-		}, {
-			where: {
-				reviewId,
-				status: AcceptReviewStatuses.PENDING,
-			}
-		})
-	}
+  async rejectAllPending(reviewId: string) {
+    return await AcceptReview.update({
+      status: AcceptReviewStatuses.REJECTED
+    }, {
+      where: {
+        reviewId,
+        status: AcceptReviewStatuses.PENDING,
+      }
+    })
+  }
 
-	async findAllPeding(reviewId: string) {
-		return await AcceptReview.findAll({
-			where: { reviewId, status: AcceptReviewStatuses.PENDING }
-		})
-	}
+  async findAllPeding(reviewId: string) {
+    return await AcceptReview.findAll({
+      where: { reviewId, status: AcceptReviewStatuses.PENDING }
+    })
+  }
 
-	async findOne(acceptReviewId: string) {
-		return await AcceptReview.findOne({
-			where: { id: acceptReviewId }
-		})
-	}
+  async findOneById(acceptReviewId: string) {
+    return await AcceptReview.findOne({
+      where: { id: acceptReviewId }
+    })
+  }
 
-	async findOneByDevAndAcceptReviewId(acceptReviewId: string, devId: string) {
-		return await AcceptReview.findOne({
-			where: { id: acceptReviewId, devId }
-		})
-	}
+  async findOneByDevAndAcceptReviewId(acceptReviewId: string, devId: string) {
+    return await AcceptReview.findOne({
+      where: { id: acceptReviewId, devId }
+    })
+  }
 
 }
--- src/modules/solution-comments/solution-comments.service.ts
+++ src/modules/solution-comments/solution-comments.service.ts
@@ -9,145 +9,145 @@ import { DefaultResponse } from 'src/common/dto/default-response.dto';
 
 @Injectable()
 export class SolutionCommentsService {
-	constructor(
-		private readonly solutionCommentsRepository: SolutionCommentsRepository,
-		private readonly reviewRequestsService: ReviewRequestService,
-		private readonly solutionService: SolutionService,
-		private readonly acceptReviewService: AcceptReviewService,
-	) { }
+  constructor(
+    private readonly solutionCommentsRepository: SolutionCommentsRepository,
+    private readonly reviewRequestsService: ReviewRequestService,
+    private readonly solutionService: SolutionService,
+    private readonly acceptReviewService: AcceptReviewService,
+  ) { }
 
-	async create(createSolutionCommentDto: CreateSolutionCommentDto): Promise<DefaultResponse> {
-		const { solutionId } = createSolutionCommentDto
+  async create(createSolutionCommentDto: CreateSolutionCommentDto): Promise<DefaultResponse> {
+    const { solutionId } = createSolutionCommentDto
 
-		const solution = await this.solutionService.findOneById(solutionId)
-		if (!solution) {
-			throw new NotFoundException('Solution not found')
-		}
+    const solution = await this.solutionService.findOneById(solutionId)
+    if (!solution) {
+      throw new NotFoundException('Solution not found')
+    }
 
-		if (solution.acceptedSolution === true) {
-			throw new UnprocessableEntityException("You can't send a new comment to a closed solution")
-		}
+    if (solution.acceptedSolution === true) {
+      throw new UnprocessableEntityException("You can't send a new comment to a closed solution")
+    }
 
-		const acceptReview = await this.acceptReviewService.findOneById(solution.acceptReviewId)
+    const acceptReview = await this.acceptReviewService.findOneById(solution.acceptReviewId)
 
-		const reviewRequest = await this.reviewRequestsService.findOneById(acceptReview.reviewId)
+    const reviewRequest = await this.reviewRequestsService.findOneById(acceptReview.reviewId)
 
-		if (createSolutionCommentDto.userId !== acceptReview.devId && createSolutionCommentDto.userId !== reviewRequest.userId) {
-			throw new UnprocessableEntityException('To comment in a solution you need to be the dev or owner')
-		}
+    if (createSolutionCommentDto.userId !== acceptReview.devId && createSolutionCommentDto.userId !== reviewRequest.userId) {
+      throw new UnprocessableEntityException('To comment in a solution you need to be the dev or owner')
+    }
 
-		const comment = await this.solutionCommentsRepository.create(createSolutionCommentDto)
+    const comment = await this.solutionCommentsRepository.create(createSolutionCommentDto)
 
-		return {
-			id: comment.id,
-			message: "Comment create successfully"
-		}
-	}
+    return {
+      id: comment.id,
+      message: "Comment create successfully"
+    }
+  }
 
-	async findAllBySolution(solutionId: string, userId: string) {
-		const solution = await this.solutionService.findOneById(solutionId)
-		if (!solution) {
-			throw new NotFoundException('Solution not found')
-		}
+  async findAllBySolution(solutionId: string, userId: string) {
+    const solution = await this.solutionService.findOneById(solutionId)
+    if (!solution) {
+      throw new NotFoundException('Solution not found')
+    }
 
-		const acceptReview = await this.acceptReviewService.findOneById(solution.acceptReviewId)
+    const acceptReview = await this.acceptReviewService.findOneById(solution.acceptReviewId)
 
-		const reviewRequest = await this.reviewRequestsService.findOneById(acceptReview.reviewId)
+    const reviewRequest = await this.reviewRequestsService.findOneById(acceptReview.reviewId)
 
-		if (userId !== acceptReview.devId && userId !== reviewRequest.userId) {
-			throw new UnprocessableEntityException('To see all comment from a solution you need to be the dev or owner')
-		}
+    if (userId !== acceptReview.devId && userId !== reviewRequest.userId) {
+      throw new UnprocessableEntityException('To see all comment from a solution you need to be the dev or owner')
+    }
 
-		return this.solutionCommentsRepository.findAllBySolution(solutionId)
-	}
+    return this.solutionCommentsRepository.findAllBySolution(solutionId)
+  }
 
-	async findOne(id: string, userId: string) {
-		const comment = await this.solutionCommentsRepository.findOneById(id)
-		if (!comment) {
-			throw new NotFoundException('Comment not found')
-		}
+  async findOne(id: string, userId: string) {
+    const comment = await this.solutionCommentsRepository.findOneById(id)
+    if (!comment) {
+      throw new NotFoundException('Comment not found')
+    }
 
-		const solution = await this.solutionService.findOneById(comment.solutionId)
-		if (!solution) {
-			throw new NotFoundException('Solution not found')
-		}
+    const solution = await this.solutionService.findOneById(comment.solutionId)
+    if (!solution) {
+      throw new NotFoundException('Solution not found')
+    }
 
-		const acceptReview = await this.acceptReviewService.findOneById(solution.acceptReviewId)
+    const acceptReview = await this.acceptReviewService.findOneById(solution.acceptReviewId)
 
-		const reviewRequest = await this.reviewRequestsService.findOneById(acceptReview.reviewId)
+    const reviewRequest = await this.reviewRequestsService.findOneById(acceptReview.reviewId)
 
-		if (userId !== acceptReview.devId && userId !== reviewRequest.userId) {
-			throw new UnprocessableEntityException('To see one comment from a solution you need to be the dev or owner')
-		}
+    if (userId !== acceptReview.devId && userId !== reviewRequest.userId) {
+      throw new UnprocessableEntityException('To see one comment from a solution you need to be the dev or owner')
+    }
 
-		return comment
-	}
+    return comment
+  }
 
-	async update(id: string, updateCommentDto: UpdateSolutionCommentDto): Promise<DefaultResponse> {
-		const comment = await this.findOne(id, updateCommentDto.userId)
-		if (!comment) {
-			throw new NotFoundException('Comment not found')
-		}
+  async update(id: string, updateCommentDto: UpdateSolutionCommentDto): Promise<DefaultResponse> {
+    const comment = await this.findOne(id, updateCommentDto.userId)
+    if (!comment) {
+      throw new NotFoundException('Comment not found')
+    }
 
-		const createdAt = new Date(comment.createdAt as string);
-		const diffMinutes = (Date.now() - createdAt.getTime()) / (1000 * 60);
+    const createdAt = new Date(comment.createdAt as string);
+    const diffMinutes = (Date.now() - createdAt.getTime()) / (1000 * 60);
 
-		if (diffMinutes > 5) {
-			throw new BadRequestException('The comment can only be edited up to 5 minutes after creation');
-		}
+    if (diffMinutes > 5) {
+      throw new BadRequestException('The comment can only be edited up to 5 minutes after creation');
+    }
 
-		if (comment.userId !== updateCommentDto.userId) {
-			throw new UnprocessableEntityException("You can't update a comment that's not your")
-		}
+    if (comment.userId !== updateCommentDto.userId) {
+      throw new UnprocessableEntityException("You can't update a comment that's not your")
+    }
 
-		const solution = await this.solutionService.findOneById(comment.solutionId)
-		if (!solution) {
-			throw new NotFoundException('Solution not found')
-		}
+    const solution = await this.solutionService.findOneById(comment.solutionId)
+    if (!solution) {
+      throw new NotFoundException('Solution not found')
+    }
 
-		if (solution.acceptedSolution === true) {
-			throw new UnprocessableEntityException("You can't update a new comment to a closed solution")
-		}
+    if (solution.acceptedSolution === true) {
+      throw new UnprocessableEntityException("You can't update a new comment to a closed solution")
+    }
 
-		await this.solutionCommentsRepository.update(id, updateCommentDto)
+    await this.solutionCommentsRepository.update(id, updateCommentDto)
 
-		return {
-			id: id,
-			message: 'Comment updated successfully'
-		}
-	}
+    return {
+      id: id,
+      message: 'Comment updated successfully'
+    }
+  }
 
-	async remove(id: string, userId: string): Promise<DefaultResponse> {
-		const comment = await this.findOne(id, userId)
-		if (!comment) {
-			throw new NotFoundException('Comment not found')
-		}
+  async remove(id: string, userId: string): Promise<DefaultResponse> {
+    const comment = await this.findOne(id, userId)
+    if (!comment) {
+      throw new NotFoundException('Comment not found')
+    }
 
-		const createdAt = new Date(comment.createdAt as string);
-		const diffMinutes = (Date.now() - createdAt.getTime()) / (1000 * 60);
+    const createdAt = new Date(comment.createdAt as string);
+    const diffMinutes = (Date.now() - createdAt.getTime()) / (1000 * 60);
 
-		if (diffMinutes > 5) {
-			throw new BadRequestException('The comment can only be removed up to 5 minutes after creation');
-		}
+    if (diffMinutes > 5) {
+      throw new BadRequestException('The comment can only be removed up to 5 minutes after creation');
+    }
 
-		if (comment.userId !== userId) {
-			throw new UnprocessableEntityException("You can't remove a comment that's not your")
-		}
+    if (comment.userId !== userId) {
+      throw new UnprocessableEntityException("You can't remove a comment that's not your")
+    }
 
-		const solution = await this.solutionService.findOneById(comment.solutionId)
-		if (!solution) {
-			throw new NotFoundException('Solution not found')
-		}
+    const solution = await this.solutionService.findOneById(comment.solutionId)
+    if (!solution) {
+      throw new NotFoundException('Solution not found')
+    }
 
-		if (solution.acceptedSolution === true) {
-			throw new UnprocessableEntityException("You can't remove a new comment to a closed solution")
-		}
+    if (solution.acceptedSolution === true) {
+      throw new UnprocessableEntityException("You can't remove a new comment to a closed solution")
+    }
 
-		await this.solutionCommentsRepository.remove(id)
+    await this.solutionCommentsRepository.remove(id)
 
-		return {
-			id: id,
-			message: 'Comment removed successfully'
-		}
-	}
+    return {
+      id: id,
+      message: 'Comment removed successfully'
+    }
+  }
 }
