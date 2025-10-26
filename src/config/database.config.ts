d3174d393250ae27a2fe07ad8d16624ea6265772
Eike Vieira <113388985+EikeRFV@users.noreply.github.com>
2025-10-25 21:16:08 -0300

    refactor(database): improve database configuration structure and use absolute imports

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,20 +6,20 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 // Import all models
-import { User } from '../modules/users/entities/user.entity';
-import { Role } from '../common/entities/role.entity';
-import { Language } from '../common/entities/language.entity';
-import { UserLanguage } from '../common/entities/user-language.entity';
-import { ReviewRequest } from '../modules/review-request/entities/review-request.entity';
-import { ReviewStatus } from '../common/entities/review-status.entity';
-import { PaymentMethods } from '../common/entities/payment-method.entity';
-import { DevStatus } from '../common/entities/dev-status.entity';
-import { AcceptReview } from '../modules/accept-review/entities/accept-review.entity';
-import { AcceptReviewStatus } from '../common/entities/accept-review-status.entity';
-import { Solution } from '../modules/solution/entities/solution.entity';
-import { Statement } from '../common/entities/statement.entity';
-import { StatementAuditLog } from '../common/entities/statement-audit-log.entity';
-import { Notification } from '../common/entities/notification.entity';
+import { User } from 'src/modules/users/entities/user.entity';
+import { Role } from 'src/common/entities/role.entity';
+import { Language } from 'src/common/entities/language.entity';
+import { UserLanguage } from 'src/common/entities/user-language.entity';
+import { ReviewRequest } from 'src/modules/review-request/entities/review-request.entity';
+import { ReviewStatus } from 'src/common/entities/review-status.entity';
+import { PaymentMethods } from 'src/common/entities/payment-method.entity';
+import { DevStatus } from 'src/common/entities/dev-status.entity';
+import { AcceptReview } from 'src/modules/accept-review/entities/accept-review.entity';
+import { AcceptReviewStatus } from 'src/common/entities/accept-review-status.entity';
+import { Solution } from 'src/modules/solution/entities/solution.entity';
+import { Statement } from 'src/common/entities/statement.entity';
+import { StatementAuditLog } from 'src/common/entities/statement-audit-log.entity';
+import { Notification } from 'src/common/entities/notification.entity';
 
 // Define models array
 const models = [
