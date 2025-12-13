ab4dd84d285365eb73b333eb3a75d0378b278a51
ecbDeveloper <developer.ecb@gmail.com>
2025-12-12 21:42:24 -0300

    ajuste modulo para poder ter auto import das entidade

--- src/modules/solution-comments/solution-comments.module.ts
+++ src/modules/solution-comments/solution-comments.module.ts
@@ -2,11 +2,21 @@ import { Module } from '@nestjs/common';
 import { SolutionCommentsController } from './solution-comments.controller';
 import { SolutionCommentsService } from './solution-comments.service';
 import { SolutionCommentsRepository } from './solution-comments.repository';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { SolutionComment } from './entities/solution-comment.entity';
+import { ReviewRequestModule } from '../review-request/review-request.module';
 import { SolutionModule } from '../solution/solution.module';
+import { AcceptReviewModule } from '../accept-review/accept-review.module';
 
 @Module({
-	controllers: [SolutionCommentsController],
-	providers: [SolutionCommentsService, SolutionCommentsRepository],
-	exports: [SolutionModule]
+  imports: [
+    SequelizeModule.forFeature([SolutionComment]),
+    ReviewRequestModule,
+    SolutionModule,
+    AcceptReviewModule,
+  ],
+  controllers: [SolutionCommentsController],
+  providers: [SolutionCommentsService, SolutionCommentsRepository],
+  exports: [SolutionCommentsService]
 })
 export class SolutionCommentsModule { }
