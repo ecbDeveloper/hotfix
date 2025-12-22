671e1e1ed00985f6320634b51086dd2ee3687564
ecbDeveloper <developer.ecb@gmail.com>
2025-12-21 23:22:40 -0300

    fix: ajusta migration da tabela accept reviews

--- src/database/migrations/20251007151046_add_table_accepts_reviews.ts
+++ src/database/migrations/20251007151046_add_table_accepts_reviews.ts
@@ -5,10 +5,20 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('accepts_reviews', table => {
     table.uuid('id').primary();
     table.integer('status_id').notNullable().defaultTo(1);
+    table.uuid('dev_id').notNullable();
+    table.uuid('review_id').notNullable();
     table.timestamps(true, true);
 
     table.foreign('status_id')
       .references('accept_review_statuses.id')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    table.foreign('dev_id')
+      .references('users.id')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    table.foreign('review_id')
+      .references('review_requests.id')
       .onDelete('CASCADE')
       .onUpdate('CASCADE');
   })
